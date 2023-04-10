deb9d28c0716536f2317991aa3092e4a6bea6c9c
achint-raj <achintya7788@gmail.com>
2023-04-11 00:59:07 +0530

    oops swap game1

---
+++ oopsswapgame1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class oopsswapgame1 {
+    public static class person{
+    int age;
+    String name;
+
+    void sayHi(){
+        System.out.println(name+"["+ age + "] sayshi");
+    }
+    }
+    public static void main(String args[]){
+       person p1 = new person();
+       p1.age = 10;
+       p1.name = "A";
+      // p1.sayHi();
+
+       person p2 = new person();
+       p2.age = 20;
+       p2.name = "B";
+      // p2.sayHi();
+
+      p1.sayHi();
+      p2.sayHi();
+
+       swap1(p1,p2);
+       p1.sayHi();
+       p2.sayHi();
+    }
+    public static void swap1(person pr1, person pr2){
+        person temp = pr1;
+        pr1=pr2;
+        pr2 = temp;
+
+
+        //as the function is over memory allocated in the heap will also be free we can say so there will be no changes
+    }
+}
